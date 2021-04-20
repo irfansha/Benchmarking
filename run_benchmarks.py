ad04e1c84665e73ca7103277d982065156c078a2
irfansha <irfansha536@gmail.com>
2021-04-20 11:57:01 +0200

    Updated comment for time limit

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
                                        0 = off
                                        1 = bloqqer (version 37)
                                        2 = bloqqer-qdo (version 37)'''),default = 0)
-  parser.add_argument("--time_limit", type=int, help="Solving time limit in seconds, default 900 seconds",default = 1800)
+  parser.add_argument("--time_limit", type=int, help="Solving time limit in seconds, default 1800 seconds",default = 1800)
 
   args = parser.parse_args()
   # Checking if directory exists:
